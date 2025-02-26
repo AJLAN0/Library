563783b1adc08ac0e23651f23e6ea09e583ac4f8
AJLAN0 <aj0ly.hd@gmail.com>
2025-02-27 00:25:47 +0300

    lab4

--- apps/bookmodule/views.py
+++ apps/bookmodule/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
-def index(request):
+def index3(request):
  name = request.GET.get("name") or "world!"
  return render(request, "bookmodule/index.html" , {"name": name}) #your render line
 
@@ -20,3 +20,15 @@ def viewbook(request, bookId):
  if book2['id'] == bookId: targetBook = book2
  context = {'book':targetBook} # book is the variable name accessible by the template
  return render(request, 'bookmodule/show.html', context)
+
+def index(request):
+ return render(request, "bookmodule/index.html")
+
+def list_books(request):
+ return render(request, 'bookmodule/list_books.html')
+
+def viewbook(request):
+ return render(request, 'bookmodule/one_book.html')
+
+def aboutus(request):
+ return render(request, 'bookmodule/aboutus.html')
